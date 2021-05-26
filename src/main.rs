4cb3636007fe620b381371197fb218364368c3dc
Richard Bradshaw <merryidleness@gmail.com>
2021-05-26 19:18:14 +0100

    Prototype.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use reqwest::blocking::Request;
+
+const URL: &str = "https://weather-broker-cdn.api.bbci.co.uk/en/observation/rss/2638077";
+
 fn main() {
-    text_weather::run()
+    text_weather::run(URL);
 }
